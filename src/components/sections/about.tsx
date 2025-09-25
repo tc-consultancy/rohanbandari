a7819196c98eaa95ce7c1fa485cdff4109c489ad
tc-consultancy <developer@tcconsultingservices.in>
2025-09-25 07:12:40 +0530

    fix: About edits

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -11,7 +11,7 @@ export default function About() {
             <div className="space-y-3 max-w-5xl  text-center mt-8 mx-auto">
                 <p className="text-3xl font-bold">Data-Driven Solutions & AI Leadership</p>
                 <p className="text-[15px] md:text-base font-medium text-neutral-800">I specialize in transforming data into tangible business value, with over three years of experience building and deploying AI/ML solutions across diverse industries. My work spans from optimizing retail operations and enhancing healthcare efficiency to securing financial systems with advanced AI.</p>
-                <p className="text-[15px] md:text-base font-medium text-neutral-800">I've consistently focused on delivering measurable impact. For example, I developed recommendation engines and demand forecasting models that boosted inventory accuracy by 35% and designed fraud detection pipelines that cut fraudulent claims by 60%, providing explainable insights to analysts.</p>
+                <p className="text-[15px] md:text-base font-medium text-neutral-800">I've consistently focused on delivering measurable impact. I developed recommendation engines and demand forecasting models that boosted inventory accuracy by 35% and designed fraud detection pipelines that cut fraudulent claims by 60%, providing explainable insights to analysts.</p>
                 <p className="text-[15px] md:text-base font-medium text-neutral-800">My foundation in this field is built on a strong academic backgrounds</p>
                 <section className="flex flex-col md:flex-row gap-12 py-4 items-center justify-center w-full">
                     <div className="h-28 flex-1 w-[95%] md:w-auto border-2 border-neutral-300 rotate-3 shadow-2xs text-start py-6 px-8 rounded-full relative">
@@ -19,8 +19,8 @@ export default function About() {
                             <div className="text-center">
                               
 
-                                <div className="text-xl font-bold">8.75</div>
-                                <div className="text-[10px] opacity-70 -mt-1">/ 10.0</div>
+                                <div className="text-xl font-bold">3.72</div>
+                                <div className="text-[10px] opacity-70 -mt-1">/ 5.0</div>
                             </div>
                         </div>
 
@@ -32,8 +32,8 @@ export default function About() {
                     <div className="h-28 flex-1 w-[95%] md:w-auto border-2 border-neutral-300 -rotate-3 shadow-2xs text-start py-6 px-8 rounded-full relative">
                         <div className="bg-black h-[60px] w-[60px] md:h-[72px] md:w-[72px] rounded-full absolute -top-9 md:-top-4 -left-4 md:left-auto md:-right-4 flex items-center justify-center text-white shadow-lg">
                             <div className="text-center">
-                            <div className="text-xl font-bold">3.72</div>
-                            <div className="text-[10px] opacity-70 -mt-1">/ 5.0</div>
+                            <div className="text-xl font-bold">8.75</div>
+                            <div className="text-[10px] opacity-70 -mt-1">/ 10.0</div>
                             </div>
                         </div>
                         <h4 className="text-lg md:text-xl leading-tight font-semibold">University of North Texas.</h4>
